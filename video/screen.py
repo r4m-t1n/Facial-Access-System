c16f06d1dd2072edc8ff7516ea6e8d68ed75e338
r4m-t1n <im.r4mt1n@gmail.com>
2025-06-03 23:53:02 +0330

    feat: add live-camera

--- video/screen.py
+++ video/screen.py
@@ -1,9 +1,9 @@
 import cv2
 
+capture = cv2.VideoCapture(0)
+
 
 async def get_webcam_frames():
-    capture = cv2.VideoCapture(0)
-
     try:
         while True:
             ret, frame = capture.read()
@@ -14,3 +14,17 @@ async def get_webcam_frames():
             yield frame
     finally:
         capture.release()
+
+async def get_frame_bytes():
+    while True:
+        ret, frame = capture.read()
+        
+        if not ret:
+            break
+        
+        _, buf = cv2.imencode(".jpg", frame)
+        frame_bytes = buf.tobytes()
+        yield (
+            b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n'
+        )

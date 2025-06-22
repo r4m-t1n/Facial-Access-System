652f42cd3df13da272ebbf8c27e2fcda1ff44cf6
r4m-t1n <im.r4mt1n@gmail.com>
2025-06-22 23:57:07 +0330

    chore: import utils.py

--- src/web/app.py
+++ src/web/app.py
@@ -2,7 +2,6 @@ import os
 import sys
 import shutil
 from typing import List
-import json
 from datetime import datetime
 import aiofiles
 from fastapi import FastAPI, Request, Form, Body, File, UploadFile, Depends, HTTPException
@@ -16,37 +15,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from model import remove_member, retrain_model
 from camera import camera_manager, get_frame_bytes
-
-from dotenv import load_dotenv
-load_dotenv()
-
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
-
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
-MEMBERS_JSON_PATH = os.path.join(DATA_DIR, 'members.json')
-MEMBERS_DATA_PATH = os.path.join(DATA_DIR, 'members_data')
-
-CAPTURED_PHOTOS = os.path.join(ROOT_DIR, "captured_photos")
-
-USERNAME = os.environ.get("LOGIN_USERNAME", "admin")
-PASSWORD = os.environ.get("LOGIN_PASSWORD", "1234")
-SECRET_KEY = os.environ.get("SECRET_KEY", "change_this_key_or_be_hacked")
-
-def get_members():
-    try:
-        os.makedirs(os.path.dirname(MEMBERS_JSON_PATH), exist_ok=True)
-        with open(MEMBERS_JSON_PATH, 'r') as file:
-            data = json.load(file)
-        return data
-    except FileNotFoundError:
-        with open(MEMBERS_JSON_PATH, 'w') as file:
-            json.dump([], file)
-        return []
-
-def write_members(members: List):
-    with open(MEMBERS_JSON_PATH, 'w') as file:
-        json.dump(members, file, indent=4)
+from utils import (
+    MEMBERS_DATA_PATH, CAPTURED_PHOTOS,
+    USERNAME, PASSWORD, SECRET_KEY,
+    get_members, write_members
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
